ddea7f94adb2feb637760be78d1be3aadc9b9888
Marisa K. Shulte <mshulte@holosphere.gq>
2020-11-20 20:05:57 +0100

    reworked join.html

---
+++ fairNet/addnetw.sh
@@ -0,0 +1,26 @@
+#
+
+netw="$1"
+tics=$(date +%s)
+regf=/.../registries/networks.log
+# Location name service:
+lns=$(echo -n "LNS" | ipfs add -Q - --hash sha3-224 --cid-base base58btc)
+echo "lns: $lns"
+qm=$(sed -e "s/network: .*/network: $netw/" charter.txt | ipfs add -Q - --hash sha3-224 --raw-leaves --cid-base base58btc)
+echo qm: $qm
+record="$tics $qm /my/networks/$netw/charter.txt"
+ipfs-log append $regf "$record"
+# publish registry file
+qm=$(ipfs files stat $regf --hash)
+ipfs-log append '/.../staged.idx' "$tics $qm $regf"
+
+webkey=$(ipfs key list -l --ipns-base=b58mh | grep -w webui | cut -d' ' -f 1)
+echo url: http://localhost:8080/ipns/$webkey/
+qm=$(ipfs files stat '/...' --hash)
+emptyd=$(ipfs object new -- unixfs-dir)
+qm=$(ipfs object patch add-link $emptyd '...' $qm)
+
+ipfs name publish /ipfs/$qm --allow-offline &
+
+
+
