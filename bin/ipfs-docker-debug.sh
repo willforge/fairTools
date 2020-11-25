e63b6cd221b4e624bb26903e1df2be0884022b69
Louise K. Molstad <lmolstad@fairTools.ml>
2020-11-25 17:11:12 +0100

    updated debug script

--- bin/ipfs-docker-debug.sh
+++ bin/ipfs-docker-debug.sh
@@ -1,10 +1,20 @@
 #
 
+uid=$(id -u); gid=$(id -g)
+IPFS_PATH=${IPFS_PATH:-$HOME/.ipfs}
 IPFS_IMAGE=${IPFS_IMAGE:-ipfs/go-ipfs}
 IPFS_CONTAINER=${IPFS_CONTAINER:-ipfs-node}
 
 docker stop $IPFS_CONTAINER
 docker rm $IPFS_CONTAINER
-docker run -it --rm -u $(id -u):$(id -g) $IPFS_IMAGE
+docker run -d --rm --name ipfs-test -u $uid:$gid $IPFS_IMAGE
+docker logs ipfs-test
+docker exec ipfs-test ipfs shutdown
+
+docker ps -a
+
+
+docker run -d --name $IPFS_CONTAINER -u $uid:$gid -v $IPFS_PATH:/data/ipfs $IPFS_IMAGE daemon
+
 
 
