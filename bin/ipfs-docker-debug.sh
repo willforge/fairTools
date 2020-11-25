3b8ac162f8985444ad4528a808e3a07e0e2a15c6
Abraham B. Mattingley <amattingley@fairTools.ml>
2020-11-25 20:18:34 +0100

    added .gitignore and variosu updates (debug)

--- bin/ipfs-docker-debug.sh
+++ bin/ipfs-docker-debug.sh
@@ -5,8 +5,6 @@ IPFS_PATH=${IPFS_PATH:-$HOME/.ipfs}
 IPFS_IMAGE=${IPFS_IMAGE:-ipfs/go-ipfs}
 IPFS_CONTAINER=${IPFS_CONTAINER:-ipfs-node}
 
-docker stop $IPFS_CONTAINER
-docker rm $IPFS_CONTAINER
 docker run -d --rm --name ipfs-test -u $uid:$gid $IPFS_IMAGE
 docker logs ipfs-test
 docker exec ipfs-test ipfs shutdown
@@ -14,7 +12,9 @@ docker exec ipfs-test ipfs shutdown
 docker ps -a
 
 
-docker run -d --name $IPFS_CONTAINER -u $uid:$gid -v $IPFS_PATH:/data/ipfs $IPFS_IMAGE daemon
+docker stop $IPFS_CONTAINER
+docker rm $IPFS_CONTAINER
+docker run -id --name $IPFS_CONTAINER -u $uid:$gid -v $IPFS_PATH:/data/ipfs $IPFS_IMAGE daemon
 
 
 
