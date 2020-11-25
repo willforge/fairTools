ef74f07086569ea015648122194bac538d009fef
Abraham B. Mattingley <amattingley@fairTools.ml>
2020-11-25 19:12:32 +0100

    fixed ports and gateways

--- bin/ipfs-docker-init.sh
+++ bin/ipfs-docker-init.sh
@@ -4,6 +4,8 @@ bindir=$(dirname "$(readlink -f "$0")")
 rootdir=$(readlink -f "${bindir}/..")
 export PERL5LIB=${PERL5LIB:-$rootdir/lib/perl5}
 export IPFS_PATH=${IPFS_PATH:-$HOME/.ipfs}
+export IPFS_STAGING=${IPFS_STAGING:-$IPFS_PATH/staging}
+if test ! -d $IPFS_STAGING; then mkdir -p $IPFS_STAGING; fi
 
 export IPFS_IMAGE=${IPFS_IMAGE:-ipfs/go-ipfs}
 export IPFS_CONTAINER=${IPFS_CONTAINER:-ipfs-node}
@@ -13,22 +15,21 @@ _ipfs() {
 }
 
 
-
 uid=$(id -u)
 gid=$(id -g)
 
 # 1. start docker if necessary ...
 if ! docker ps -f name=$IPFS_CONTAINER | grep -q -w $IPFS_CONTAINER; then
-if ! docker ps -a -f name=$IPFS_CONTAINER | grep -q -w $IPFS_CONTAINER; then
-echo "docker: run $IPFS_IMAGE in $IPFS_CONTAINER"
-docker run -d --name $IPFS_CONTAINER --user $uid:$gid $IPFS_IMAGE
-else
-echo "docker: start $IPFS_CONTAINER"
-docker start $IPFS_CONTAINER
+  if docker ps -a -f name=$IPFS_CONTAINER | grep -q -w $IPFS_CONTAINER; then
+     echo "docker: rm $IPFS_CONTAINER"
+     docker rm $IPFS_CONTAINER
+  fi
+  docker run -d --name $IPFS_CONTAINER --user $uid:$gid \
+             -v $IPFS_PATH:/data/ipfs -w /export $IPFS_IMAGE daemon
+  sleep 7
+  docker logs $IPFS_CONTAINER
 fi
-sleep 7
-docker logs $IPFS_CONTAINER | waitfor 'ready$'
-fi
+
 docker ps -a -f name=$IPFS_CONTAINER
 
 
@@ -49,17 +50,17 @@ fi
 
 
 origin=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | json_xs -e '$_ = $_->[0]' | sed -e 's/"//g')
-gwport=$gw_port
-gwhost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $gw_port | sed -e 's,.*https*://,,' -e "s/:$gw_port.*//")
+gwport=8080
+gwhost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $gwport | head -1 | sed -e 's,.*https*://,,' -e "s/:$gwport.*//")
 if [ "x$gwhost" = 'x' ]; then
- gwport=8080
- gwhost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $gwport | sed -e 's,.*https*://,,' -e "s/:$gwport.*//")
+  gwport=$gw_port
+  gwhost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $gw_port | head -1 | sed -e 's,.*https*://,,' -e "s/:$gw_port.*//")
 fi
-apiport=$api_port
-apihost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $api_port | sed -e 's,.*https*://,,' -e "s/:$api_port.*//")
+apiport=5001
+apihost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $apiport | head -1 | sed -e 's,.*https*://,,' -e "s/:$apiport.*//")
 if [ "x$apihost" = 'x' ]; then
- apiport=5001
- apihost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $apiport | sed -e 's,.*https*://,,' -e "s/:$apiport.*//")
+  apiport=$api_port
+  apihost=$(_ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin | grep $api_port | head -1 | sed -e 's,.*https*://,,' -e "s/:$api_port.*//")
 fi
 
 echo "gw: $gwhost:$gwport -> $gw_port"
@@ -72,14 +73,14 @@ docker rm $IPFS_CONTAINER
 set -x
 docker run -d --name $IPFS_CONTAINER \
   -v $IPFS_STAGING:/export -v $IPFS_PATH:/data/ipfs -w /export \
-  -u $uid:$gig \
+  -u $uid:$gid \
   -p 4001:$swarm_port/tcp \
   -p 4001:$swarm_port/udp \
   -p $gwhost:$gwport:$gw_port \
   -p $apihost:$apiport:$api_port \
   $IPFS_IMAGE daemon
 
-docker logs --until 59s $IPFS_CONTAINER | waitfor 'ready$'
+docker logs --until 59s $IPFS_CONTAINER
 
 docker ps -a -f name=$IPFS_CONTAINER
 
