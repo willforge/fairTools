198a7788a1c13306ed07f2fdc561cc5b10a95231
Malinda C. Ueki <mueki@bigtm.gq>
2020-11-30 16:30:12 +0100

    added a script for just providing the "nid" for the log

---
+++ wlog/prov-log.sh
@@ -0,0 +1,18 @@
+# 
+
+if ! which ipfs 2>/dev/null; then
+ipfs() {
+  docker exec -i ipfs-node ipfs "$@"
+}
+fi
+set PATH=../bin:$PATH
+
+key='clef-secrete'
+label='a-big-log.txt'
+urn="urn:wwlog:$label"
+sha2=$(echo -n "$key,$label" | openssl sha256 | cut -d' ' -f2)
+nid=$(echo $sha2 | cut -c -13)
+
+ping=$(echo -n $nid | ipfs add -Q --pin=true --raw-leaves --hash sha3-224 --cid-base base58btc)
+echo ping: $ping
+
