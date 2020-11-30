0a4dd1852e6e09fadbf98a1b09d4f423f8f863d7
Malinda C. Ueki <mueki@bigtm.gq>
2020-11-30 17:45:54 +0100

    added docekrized ipfs() in joinfairteam

--- bin/joinfairteam.sh
+++ bin/joinfairteam.sh
@@ -1,5 +1,16 @@
 # 
 
+# dockerized ipfs:
+export IPFS_CONTAINER=${IPFS_CONTAINER:-ipfs-node}
+export IPFS_PATH=${IPFS_PATH:-$HOME/.ipfs}
+export IPFS_STAGING=${IPFS_STAGING:-$IPFS_PATH/staging}
+echo IPFS_PATH: $IPFS_PATH
+if ! which ipfs 2>/dev/null; then
+ipfs() {
+  docker exec -i $IPFS_CONTAINER ipfs "$@"
+}
+fi
+
 
 qm=$(echo -n 'fairTeam' | ipfs add -Q --raw-leaves --hash sha3-224 --cid-base base58btc)
 echo "ping: $qm"
