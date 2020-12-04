27a54e985cd28be25b5fd211a5b565ec4ef0bed7
Ronnie L. Decillis <rdecillis@csap.ml>
2020-12-04 16:47:44 +0100

    added FAIRTOOLS detection and PERL5LIB setting in envrc.sh

--- bin/envrc.sh
+++ bin/envrc.sh
@@ -1,6 +1,28 @@
 #!/bin/echo source
 
 # intent: setting ft environment
+if [ "x$bindir" = 'x' ]; then
+if echo "$-" | grep -q 'i'; then
+cmd=$(history | tail -1 | sed -e 's/.*://' -e 's/^[^ ]*/echo/' )
+zero=$(eval "$cmd" | cut -d' ' -f2);
+wrapper="$(readlink -f "$zero")"
+#set $zero $*
+else
+wrapper="$(readlink -f "$0")"
+fi
+bindir=$(dirname "$wrapper")
+
+fi
+rootdir=$(readlink -f "${bindir}/..")
+export FAIRTOOLS_PATH=$rootdir
+export PERL5LIB=${PERL5LIB:-$FAIRTOOLS_PATH/lib/perl5}
+if echo "$-" | grep -q '.*i.*'; then
+export PATH=$PERL5LIB/bin:$bindir:$PATH
+else
+export PATH=$PERL5LIB/bin:/usr/local/bin:/usr/bin:$bindir:/bin
+fi
+
+
 
 #if ! which ipms 1>/dev/null 2&>1; then
 # alias ipms='ipfs'
