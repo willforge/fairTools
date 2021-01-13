e6ee04906018da4f84ea597bac4bd6a7ad6f7b05
Dawn T. Kats <dkats@gcfs.ml>
2021-01-13 07:11:15 +0100

    pushing ft-envrc.sh for less verbosity

--- ftools/ft-envrc.sh
+++ ftools/ft-envrc.sh
@@ -1,15 +1,17 @@
 # fairTools runtime ENVIRONMENT
 
+if [ "x$FAIRTOOLS_PATH" = 'x' ]; then
+what=envrc
 core="${core:-fair}"
 fname="${0##*/}"
 prefix=${fname%%-*}
-echo "--- # ${prefix}-envrc.sh (caller $0)"
+
+echo "--- # ${prefix}-$what.sh (caller $0)"
 
 export XDG_CACHE_HOME="$HOME/.cache"
 
 FTCONFIG=$LOCALDIR/${core}Tools/${prefix}-config.sh
 
-if [ "x$FAIRTOOLS_PATH" = 'x' ]; then
 
 cli=$(which ${prefix})
 root_cli=$(readlink $cli)
@@ -23,8 +25,8 @@ CLIDIR=$(dirname "$wrapper")
 echo "CLIDIR: $CLIDIR"
 LOCALDIR=$(readlink -m $CLIDIR/..)
 
-INSTALLDIR=${cli%%/bin/*}
-ROOTDIR="${root_cli%%/opt/*}"
+export INSTALLDIR=${cli%%/bin/*}
+export ROOTDIR="${root_cli%%/opt/*}"
 
 # calling script
 self=$(readlink -f $0)
@@ -32,7 +34,7 @@ CALLINGDIR=$(dirname $self)
 
 # fairTools ENVIRONMENT
 #export FAIRTOOLS_PATH=${FAIRTOOLS_PATH:-$ROOTDIR/opt/${core}Tools}
-FAIRTOOLS_PATH=$(readlink -m "${CALLINGDIR}/..")
+export FAIRTOOLS_PATH=$(readlink -m "${CALLINGDIR}/..")
 
 echo "ROOTDIR/bin: $ROOTDIR/bin"
 echo "LOCALDIR/bin: $LOCALDIR/bin"
@@ -44,12 +46,13 @@ PATH="$PATH:$INSTALLDIR/bin" # for install only
 PATH="$PATH:$ROOTDIR/bin:$LOCALDIR/bin:$FAIRTOOLS_PATH/bin:$CALLINGDIR"
 export PATH;
 
-fi
 
 export PERL5LIB=$ROOTDIR/lib/perl5
 PERL_MB_OPT="--install_base \"$ROOTDIR\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=$ROOTDIR"; export PERL_MM_OPT;
 
 export IPFS_CONTAINER=ipfs-node
+
+fi
 
 # --- all edits below this line are subject to be erased !
