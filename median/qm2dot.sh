93ac099b18260b08da6d5cc79bf4429bb69da3aa
Kathrine K. Burnie <kburnie@happycoin.cf>
2021-02-01 16:26:06 +0100

    switched to curl instead of ipfs

--- median/qm2dot.sh
+++ median/qm2dot.sh
@@ -10,11 +10,17 @@ ipfs() {
 
 fi
 
+echo please enter a qm ...
+
 while true; do
 read url
 qmdot="${url##*/}"
-ipfs get -o jtree.dot $qmdot
+#ipfs get -o jtree.dot $qmdot
+echo fetching jtree.dot
+curl -s -o jtree.dot http://127.0.0.1:8080/ipfs/$qmdot
+ls -l jtree.dot
 dot -Tpng jtree.dot -o jtree.png;
+echo eog jtree.png
 done
 
 #eog jtree.png
