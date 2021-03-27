d20f9f309391e0c34c5c315f412bd9b554cef29a
Marie V. Dewinne <mdewinne@hltm.ml>
2021-03-27 16:45:03 +0100

    added a reboot script for ipfs-docker

---
+++ bin/ipfs-docker-reboot.sh
@@ -0,0 +1,34 @@
+#
+
+bindir=$(dirname "$(readlink -f "$0")")
+rootdir=$(readlink -f "${bindir}/..")
+export PERL5LIB=${PERL5LIB:-$rootdir/lib/perl5}
+export IPFS_PATH=${IPFS_PATH:-$HOME/.ipfs}
+
+export IPFS_IMAGE=${IPFS_IMAGE:-ipfs/go-ipfs}
+export IPFS_CONTAINER=${IPFS_CONTAINER:-ipfs-node}
+
+uid=$(id -u); gid=$(id -g)
+
+
+# shutdown IPFS_CONTAINER
+docker exec -i $IPFS_CONTAINER ipfs shutdown
+sleep 5;
+
+# start docker ..
+if ! docker ps -f name=$IPFS_CONTAINER | grep -q -w $IPFS_CONTAINER; then
+  if ! docker ps -a -f name=$IPFS_CONTAINER | grep -q -w $IPFS_CONTAINER; then
+    echo "docker: run $IPFS_IMAGE in $IPFS_CONTAINER"
+    docker run -d --name $IPFS_CONTAINER --user $uid:$gid $IPFS_IMAGE
+  else
+    echo "docker: start $IPFS_CONTAINER"
+    docker start $IPFS_CONTAINER
+  fi
+  sleep 7
+else
+  echo "docker: $IPFS_CONTAINER already running"
+fi
+docker ps -a -f name=$IPFS_CONTAINER
+
+docker logs --since 59s $IPFS_CONTAINER 
+
