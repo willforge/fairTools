35c3afa982dc93ed0f1a4fbcd2c15288454c674d
Julianne M. Fosher <jfosher@hltm.ml>
2021-04-26 19:05:08 +0200

    use a CJS module within an AMD's define()

---
+++ modules/libraryRCJS.js
@@ -0,0 +1,30 @@
+define(function(require,exports,module) {
+
+// Area function
+let area = function (length, breadth) {
+    let a = length * breadth;
+    console.log('Area of the rectangle is ' + a + ' square unit');
+    return a;
+}
+  
+// Perimeter function
+let perimeter = function (length, breadth) {
+    let p = 2 * (length + breadth);
+    console.log('Perimeter of the rectangle is ' + p + ' unit');
+    return p;
+}
+  
+// Making all functions available in this
+// module to exports that we have made
+// so that we can import this module and
+// use these functions whenever we want.
+/*
+exports.area = area;
+exports.perimeter = perimeter;
+*/
+module.exports = {
+    area,
+    perimeter
+}
+
+});
